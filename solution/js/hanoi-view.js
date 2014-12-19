00c8176d5d6099a4d1ddfcc8fbf4d63d61bf221d
Jonathan Tamboer <jonathanlemuel@me.com>
2014-12-19 12:08:24 -0500

    Switch from re-rendering html to only toggling classes.

--- solution/js/hanoi-view.js
+++ solution/js/hanoi-view.js
@@ -12,11 +12,13 @@
       "ul",
       this.clickTower.bind(this)
     );
+
+    this.setupTowers();
     this.render();
   };
 
   View.prototype.clickTower = function (event) {
-    var clickedTowerIdx = parseInt($(event.currentTarget).data("tower"));
+    var clickedTowerIdx = $(event.currentTarget).index();
 
     if (this.fromTowerIdx == null) {
       this.fromTowerIdx = clickedTowerIdx;
@@ -31,40 +33,70 @@
     this.render();
 
     if (this.game.isWon()) {
-      // Remove click handler.
+      // Remove click handler when done.
       this.$el.off("click");
+      this.$el.addClass("game-over");
       alert("Good work, you!");
     }
   };
 
-  View.prototype.render = function () {
+  View.prototype.setupTowers = function () {
+    /*
+    We're setting up the skeleton for our towers
+    here. It consist of three <ul> elements, all
+    floated left, with each three nested <li>s.
+    Because the <ul>s are floated, we need to
+    add the `.group` class, containing the clearfix,
+    to their parent. The <li> elements all will be
+    invisible by default. Adding a disk class to
+    them will make them visible.
+    */
+
     this.$el.empty();
-    // We need this class to clear our floated towers.
     this.$el.addClass("group");
 
+    var $tower, $disk;
+
     for (var towerIdx = 0; towerIdx < 3; towerIdx++) {
-      this.$el.append(this.renderTower(towerIdx));
+      $tower = $("<ul>");
+
+      for (var diskIdx = 0; diskIdx < 3; diskIdx++) {
+        $disk = $("<li>");
+        $tower.append($disk);
+      }
+
+      this.$el.append($tower);
     };
   };
 
-  View.prototype.renderTower = function (towerIdx) {
-    var $towerEl = $("<ul>").data("tower", towerIdx);
+  View.prototype.render = function () {
+    /*
+    Rather than removing all our elements from the page
+    and building them up again, we are removing only the
+    classes and re-adding them as necessary. This is a
+    more light-weight approach and will speed up the
+    redrawing in the browser.
+    */
+    var $towers = this.$el.find("ul");
+    $towers.removeClass();
 
-    if (this.fromTowerIdx == towerIdx) {
-      $towerEl.addClass("selected");
+    if (this.fromTowerIdx !== null) {
+      $towers.eq(this.fromTowerIdx).addClass("selected");
     }
 
-    this.game.towers[towerIdx].forEach(function (diskWidth) {
-      var $diskEl = $("<li>").addClass("disk-" + diskWidth);
-      $towerEl.prepend($diskEl);
+    this.game.towers.forEach(function(disks, towerIdx){
+      var $disks = $towers.eq(towerIdx).children();
+      $disks.removeClass();
+
+      disks.forEach(function(diskWidth, diskIdx) {
+        /*
+        Since our disks are stacked from bottom to top
+        as [3, 2, 1], we have to select from the back
+        of our jQuery object, using negative indices.
+        */
+        $disks.eq(-1 * (diskIdx + 1)).addClass("disk-" + diskWidth);
+      });
     });
-
-    while ($towerEl.children().length < 3) {
-      // Use a fake disk to force bottom alignment.
-      var $fakeDisk = $("<li>");
-      $towerEl.prepend($fakeDisk);
-    };
-
-    return $towerEl;
   };
+
 })();
